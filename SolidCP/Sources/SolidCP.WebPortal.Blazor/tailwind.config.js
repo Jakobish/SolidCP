38d034e2bab2ad750f00fe1d49491de522c4ebaf
Jakobish <kobi@virtualbox.co.il>
2025-12-05 02:13:53 +0200

    feat: Introduce SolidCP.WebPortal.Blazor project with initial page and frontend dependencies, and update SolidCP.WebPortal.Modern's package lock.

---
+++ SolidCP/Sources/SolidCP.WebPortal.Blazor/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./Pages/**/*.cshtml",
+    "./Views/**/*.cshtml",
+    "./wwwroot/**/*.html",
+    "./**/*.razor"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
